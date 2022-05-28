ee216811753e0c43611d11155c3c462faf1537f7
Jeison Caguana <jeisoncaguana@gmail.com>
2022-05-28 06:30:48 -0500

    Añado archivo public

---
+++ src/public/ListaUsuarios.ts
@@ -0,0 +1,12 @@
+export const ListaUsuarios = [
+    {
+        id:1,
+        name:"Juan",
+        birth_date :"20-10-1999"
+    },
+    {
+        id:2,
+        name:"Lucho",
+        birth_date :"21-05-1997"
+    }
+]
